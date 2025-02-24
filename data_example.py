c7002967e7d113cfb0d5090611653f67543b8773
Joachim <joachimsh91@gmail.com>
2025-02-24 03:37:48 +0100

    first test commit

---
+++ data_example.py
@@ -0,0 +1,8 @@
+'''Contains lists of public and personal data used for scraper and mailer'''
+
+URL_LINK = 'https://en.example.com&page=' # URL of website to scrape
+JOB_DB = r'C:\Users\<username>\repos\job_finder\database\jobdata.sqlite' # Path/name of database
+JOB_COL = ['Title', 'Company', 'Salary', 'Period', 'Location'] # Columns used for Pandas
+MAIL_ADDRESS = ['sender_mail@gmail.com', 'reciever_mail@gmail.com'] # Sender and reciever mail address
+MAIL_PASSWORD = ['<sender_mail app password'] # App password for sender email
+ALL_DATA = [] # Temporary storage of scraped data to be used for data processing
